a9f9e5d837f544402159c8e8484916126fc3d3e0
Asker Khuzhokov <a7kerkh@yandex.ru>
2025-05-09 02:49:54 +0300

    chore: update arc

--- lua/plugins/arc.lua
+++ lua/plugins/arc.lua
@@ -2,11 +2,12 @@
 
 return {
 	"askerdev/arc.nvim",
-	event = { "BufEnter", "BufWinEnter" },
-	dependencies = { "folke/snacks.nvim" },
+	lazy = false,
+	dependencies = { "folke/snacks.nvim", "MunifTanjim/nui.nvim" },
 	opts = {},
 	-- stylua: ignore
 	keys = {
+		{ "<leader>hr", function() Arc.signs.hunk_reset() end, desc = "Arc Hunk Reset" },
 		{ "<leader>hp", function() Arc.signs.hunk_preview() end, desc = "Arc Hunk Preview" },
 		{ "<leader>fg", function() Arc.picker.files() end, desc = "Arc Files" },
 		{ "<leader>gb", function() Arc.picker.branches() end, desc = "Arc Branches" },
